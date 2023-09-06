a54d3db576586114d33a0e3edc757031a03e3765
yashmalviya89 <yash.malviya@tothenew.com>
2023-09-06 16:00:34 +0530

    With comments ToDoList with MySql database

--- ToDoList.py
+++ ToDoList.py
@@ -1,19 +1,25 @@
 import mysql.connector as connector
 from datetime import datetime
 
+# Create a DBHelper class for managing tasks in a MySQL database
 class DBHelper:
     def __init__(self):
-        self.con = connector.connect(host='localhost',
-                                     port='3306',
-                                     user='root',
-                                     password='Jordan@yash6989',
-                                     database='pythontodolist')
+        # Initialize a connection to the MySQL database
+        self.con = connector.connect(
+            host='localhost',
+            port='3306',
+            user='root',
+            password='Jordan@yash6989',
+            database='pythontodolist'
+        )
 
+        # Create a table for tasks if it does not exist
         query = "create table if not exists todolist(taskId int primary key auto_increment,taskTitle varchar(200),createdAt datetime,completedAt datetime,status varchar(200) default 'incomplete')"
         cur = self.con.cursor()
         cur.execute(query)
         print("Table Created Successfully...")
 
+    # Method to insert a new task into the database
     def insert_task(self, taskTitle, status):
         # Check if a task with the same title and incomplete status exists
         check_query = "select taskId from todolist where taskTitle = '{}' and status = '{}'".format(taskTitle, status)
@@ -25,13 +31,13 @@ class DBHelper:
             print("Task with the same title and incomplete status exists.")
             return
 
-        # Insert the task
+        # Insert the task into the database
         insert_query = "insert into todolist(taskTitle, createdAt, status) values('{}', '{}', '{}')".format(taskTitle, str(datetime.now()), status)
         cur.execute(insert_query)
         self.con.commit()
         print("Task Inserted to DB")
 
-    # Fetch all: Show data from the table
+    # Method to fetch and display tasks from the database
     def show_data(self, status=None):
         query = "select * from todolist"
 
@@ -52,8 +58,9 @@ class DBHelper:
             print("status : ", row[4])
             print()
 
+    # Method to edit a task in the database
     def edit_data(self, taskId, newTitle, newStatus):
-         # Check if a task with the same title and incomplete status exists
+        # Check if a task with the same title and incomplete status exists
         check_query = "select taskId from todolist where taskTitle = '{}' and status = '{}'".format(newTitle, newStatus)
         cur = self.con.cursor()
         cur.execute(check_query)
@@ -68,6 +75,7 @@ class DBHelper:
         print("Data Updated Successfully..")
         print(f"newTitle : {newTitle} and newStatus : {newStatus}")
 
+    # Method to mark a task as complete in the database
     def mark_complete(self, taskId):
         check_query = "select * from todolist where taskId = '{}' and status = 'complete' and not exists(select 1 from todolist where taskId = '{}')".format(taskId,taskId)
         cur = self.con.cursor()
@@ -82,6 +90,7 @@ class DBHelper:
         self.con.commit()
         print("Task Marked as Complete.")
 
+    # Method to delete a task from the database
     def delete_data(self, taskId):
         query = "delete from todolist where taskId = '{}'".format(taskId)
         cur = self.con.cursor()
@@ -89,6 +98,7 @@ class DBHelper:
         self.con.commit()
         print("Data deleted successfully...")
 
+    # Method to search for tasks by title in the database
     def search_by_title(self, taskTitle):
         query = "select * from todolist where taskTitle like '%{}%'".format(taskTitle)
         cur = self.con.cursor()
--- main.py
+++ main.py
@@ -1,13 +1,17 @@
-from ToDoList import DBHelper
-import argparse
-from datetime import datetime
+# Import necessary modules and classes
+from ToDoList import DBHelper 
+import argparse  
+from datetime import datetime 
 
 def main():
     db = DBHelper()
 
+    # Create an ArgumentParser object to handle command-line arguments
     parser = argparse.ArgumentParser(description="TODO List Manager")
+
+    # Define command-line arguments and their descriptions
     parser.add_argument('--create', help="Create a new task with title")
-    parser.add_argument('--edit', nargs=3, help="Edit task title and task status. Provide task ID , new title and new status")
+    parser.add_argument('--edit', nargs=3, help="Edit task title and task status. Provide task ID, new title, and new status")
     parser.add_argument('--mark-complete', help="Mark a task as complete. Provide task ID")
     parser.add_argument('--delete', help="Delete a task. Provide task ID")
     parser.add_argument('--list', choices=['all', 'incomplete', 'complete'], help="List tasks")
@@ -15,23 +19,32 @@ def main():
     args = parser.parse_args()
 
     try:
+        # Check the provided command-line arguments and execute the corresponding actions
         if args.create:
-            db.insert_task(args.create,"incomplete")
+            # Create a new task with the provided title and default status 'incomplete'
+            db.insert_task(args.create, "incomplete")
         elif args.edit:
+            # Edit a task by providing task ID, new title, and new status
             task_id, new_title, new_status = args.edit
             db.edit_data(task_id, new_title, new_status)
         elif args.mark_complete:
+            # Mark a task as complete by providing the task ID
             db.mark_complete(args.mark_complete)
         elif args.delete:
+            # Delete a task by providing the task ID
             db.delete_data(args.delete)
         elif args.list:
             if args.list == 'all':
+                # List all tasks
                 db.show_data()
             else:
+                # List tasks based on the specified status (incomplete or complete)
                 db.show_data(args.list)
         elif args.search:
+            # Search for tasks by title
             db.search_by_title(args.search)
         else:
+            # Display help message if no valid arguments are provided
             parser.print_help()
     except Exception as e:
         print(e)
@@ -39,4 +52,4 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    main()  
